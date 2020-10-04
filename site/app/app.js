73bd73ebe28f088c7f5f6b51b9ff8bc5f824bd02
safak <safak.tarazan@nl.abnamro.com>
2020-10-04 12:18:58 +0200

    init

---
+++ site/app/app.js
@@ -0,0 +1,31 @@
+var app = angular.module("myApp", ["ngRoute"]);
+app.config(function($routeProvider) {
+    $routeProvider
+    .when("/", {
+        templateUrl : "views/home.html",
+        controller : "controllers/homeCtrl"
+    })
+    .when("/home", {
+        templateUrl : "views/home.html",
+        controller : "controllers/homeCtrl"
+    }).when("/services", {
+        templateUrl : "views/services.html",
+        controller : "controllers/servicesCtrl"
+    }).when("/automation", {
+        templateUrl : "views/automation.html",
+        controller : "controllers/automationCtrl"
+    }).when("/cybersecurity", {
+        templateUrl : "views/cybersecurity.html",
+        controller : "controllers/cybersecurityCtrl"
+    }).when("/devsecops", {
+        templateUrl : "views/devsecops.html",
+        controller : "controllers/devsecopsCtrl"
+    }).when("/about", {
+        templateUrl : "views/about.html",
+        controller : "controllers/aboutCtrl"
+    }).when("/contact", {
+        templateUrl : "views/contact.html",
+        controller : "controllers/contactCtrl"
+    });
+});
+
---
+++ site/app/controllers/cybersecurityCtrl.js
@@ -0,0 +1,3 @@
+app.controller("cybersecurityCtrl", function ($scope) {
+
+});
