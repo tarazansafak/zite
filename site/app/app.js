3caf724375d7617bdd813f6b51491f6825898c0b
safak <safak.tarazan@nl.abnamro.com>
2020-10-15 13:34:12 +0200

    corpspeed

--- site/app/app.js
+++ site/app/app.js
@@ -1,40 +1,45 @@
 var app = angular.module("myApp", ["ngRoute"]);
 
 
-
-
-
-app.config(function($routeProvider) {
+app.config(function ($routeProvider) {
     $routeProvider
-    .when("/", {
-        templateUrl : "views/home.html",
-        controller : "controllers/homeCtrl"
-    })
-    .when("/home", {
-        templateUrl : "views/home.html",
-        controller : "controllers/homeCtrl"
-    }).when("/services", {
-        templateUrl : "views/services.html",
-        controller : "controllers/servicesCtrl"
+        .when("/", {
+            templateUrl: "views/home.html",
+            controller: "controllers/homeCtrl"
+        })
+        .when("/home", {
+            templateUrl: "views/home.html",
+            controller: "controllers/homeCtrl"
+        }).when("/services", {
+        templateUrl: "views/services.html",
+        controller: "controllers/servicesCtrl"
     }).when("/automation", {
-        templateUrl : "views/automation.html",
-        controller : "controllers/automationCtrl"
+        templateUrl: "views/automation.html",
+        controller: "controllers/automationCtrl"
     }).when("/cybersecurity", {
-        templateUrl : "views/cybersecurity.html",
-        controller : "controllers/cybersecurityCtrl"
+        templateUrl: "views/cybersecurity.html",
+        controller: "controllers/cybersecurityCtrl"
     }).when("/devsecops", {
-        templateUrl : "views/devsecops.html",
-        controller : "controllers/devsecopsCtrl"
+        templateUrl: "views/devsecops.html",
+        controller: "controllers/devsecopsCtrl"
     }).when("/about", {
-        templateUrl : "views/about.html",
-        controller : "controllers/aboutCtrl"
+        templateUrl: "views/about.html",
+        controller: "controllers/aboutCtrl"
+    }).when("/spikesite", {
+        templateUrl: "views/spikesite.html",
+        controller: "controllers/spikesiteCtrl"
     }).when("/contact", {
-        templateUrl : "views/contact.html",
-        controller : "controllers/contactCtrl"
+        templateUrl: "views/contact.html",
+        controller: "controllers/contactCtrl"
     });
+
 });
 app.controller("controllers/homeCtrl", function ($scope) {
 });
 app.controller("controllers/contactCtrl", function ($scope) {
 });
+
+app.controller("controllers/spikesiteCtrl", function ($scope) {
+});
+
 
